e2250dc1315d7fe3129a73b906c7ae1fa8883005
Philip2BautistaV <philip.ii.v.bautista@accenture.com>
2026-07-19 16:52:43 +0800

    Update root endpoint response message

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "AIML in develop");
+app.MapGet("/", () => "AIML in develop on Sunday");
 
 app.Run();
